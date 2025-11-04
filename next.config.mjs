cc8b66be2e5204b04168de2b489f01880d6a7b92
loftyabhi <abhisekshaw0206@gmail.com>
2025-11-04 15:43:42 +0530

    config fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,29 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'pump.mypinata.cloud',
-        port: '',
+        protocol: "https",
+        hostname: "pump.mypinata.cloud",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "router.huggingface.co",
+        port: "",
       },
     ],
   },
-  
+
+  // Optional: helpful to prevent 404 on dynamic routes
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["*"],
+    },
+  },
 };
 
 export default nextConfig;
